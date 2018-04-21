0c3cdcd88eef55e3a4869b40cc5ae333ca0cd804
YutaoZhou <taogezz@live.com>
2018-04-21 09:34:51 +0800

    modified:   GetBook.py

--- GetBook.py
+++ GetBook.py
@@ -10,10 +10,10 @@ import time
 Max = 2549
 BookName = "Linux Server"
 for i in range (0,Max):
-    imageName = BookName + ('_%04d.JPG' % i)
+    imageName = BookName + ('_%04d.JPEG' % i)
     print imageName
-    os.system('adb shell screencap -p /sdcard/'+ imageName)
-    os.system('adb pull /sdcard/'+imageName+' .')
-    os.system('adb shell input swipe 1000 500 0 500 100')
+    run('adb shell screencap -p /sdcard/'+ imageName,shell=True)
+    run('adb pull /sdcard/'+imageName+' .',shell=True)
+    run('adb shell input swipe 1000 500 0 500 100',shell=True)
     #time.sleep(0.1)
-    adb.run('adb shell rm -rf /sdcard/'+imageName)
+    run('adb shell rm -rf /sdcard/'+imageName,shell=True)
