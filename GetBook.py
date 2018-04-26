75637b8893205d656e934f64bd9cd810c435219c
YutaoZhou <taogezz@live.com>
2018-04-26 20:12:39 +0800

    FA

--- GetBook.py
+++ GetBook.py
@@ -7,13 +7,27 @@ import platform
 import time
 
 
-Max = 2549
-BookName = "Linux Server"
-for i in range (0,Max):
+class auto_adb():    
+    def run(self, raw_command):
+        command = '{} {}'.format('adb', raw_command)
+        process = os.popen(command)
+        output = process.read()
+        #print output
+        return output
+
+    def adb_path(self):
+        return self.adb_path
+    
+
+adb = auto_adb()
+Max = 2550
+BookName = "Nginx"
+for i in range (1134,Max):
     imageName = BookName + ('_%04d.JPEG' % i)
-    print imageName
-    run('adb shell screencap -p /sdcard/'+ imageName,shell=True)
-    run('adb pull /sdcard/'+imageName+' .',shell=True)
-    run('adb shell input swipe 1000 500 0 500 100',shell=True)
+    print 'Creating' + imageName
+    adb.run('shell screencap -p /sdcard/'+ imageName)
+    adb.run('pull /sdcard/'+imageName+' .')
+    adb.run('shell input swipe 1000 500 0 500 100')
     #time.sleep(0.1)
-    run('adb shell rm -rf /sdcard/'+imageName,shell=True)
+    adb.run('shell rm -rf /sdcard/'+imageName)
+    print 'Pull' + imageName + 'Success!'
